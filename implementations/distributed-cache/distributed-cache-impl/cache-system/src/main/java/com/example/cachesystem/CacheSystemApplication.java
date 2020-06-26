265dff989455d9d15a8409da3796ca708a9883c3
rounak-sema4 <rounak.raj@sema4genomics.com>
2020-06-26 11:29:49 +0530

    add modules

---
+++ implementations/distributed-cache/distributed-cache-impl/cache-system/src/main/java/com/example/cachesystem/CacheSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheSystemApplication.class, args);
+	}
+
+}
