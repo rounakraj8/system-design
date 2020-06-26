73c4ba7ea8cfccf3f522624a698cd70f4ce41402
rounak-sema4 <rounak.raj@sema4genomics.com>
2020-06-26 12:33:02 +0530

    rename package

--- implementations/distributed-cache/distributed-cache-impl/cachesystem/src/test/java/com/systemdesign/distributedcache/cachesystem/DemoApplicationTests.java
+++ implementations/distributed-cache/distributed-cache-impl/cachesystem/src/test/java/com/systemdesign/distributedcache/cachesystem/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cachesystem;
+package com.systemdesign.distributedcache.cachesystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
